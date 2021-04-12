c5dc71208d33e3df68d91e4bf3dd5cd79a0640f7
mossan-hoshi <sackn5@gmail.com>
2021-04-13 00:03:49 +0900

    add add1bit modules

--- src/example2/add1bit.v
+++ src/example2/add1bit.v
@@ -1,11 +1,32 @@
-module add1bit(
-    input  A,
-    input  B,
-    output O,
-    output C
+module add(
+    input  [BIT_NUM-1:0] A,
+    input  [BIT_NUM-1:0] B,
+    output [BIT_NUM-1:0] O,
+    output               C_o
 );
 
-assign O = A ^ B;
-assign C = A & B;
+parameter BIT_NUM = 8;
+
+wire O_1;
+wire O_2;
+wire C_1;
+wire C_2;
+
+
+genvar i;
+generate 
+    for(i = 1; i < BIT_NUM; i = i + 1) begin: 
+        add add_1 (
+            .A   (A[i]  ),
+            .B   (B[i]  ),
+            .C_i (C_i[i] )
+            .O   (O_1   ),
+            .C_o (C_1)
+        );
+    }
+endgenerate
+
+assign O = O_2;
+assign C_o = C_2 | C_1;
 
 endmodule
--- src/example2/test_add1bit_half.v
+++ src/example2/test_add1bit_half.v
@@ -1,37 +1,34 @@
 `timescale 1ps/1ps
 
-module test_add1bit();
+module test_add1bit_half();
     parameter CLOCK_CYCLE = 100;
-    parameter COUNTER_BIT_NUM = 3;
+    parameter COUNTER_BIT_NUM = 2;
     parameter SIMULATION_PERIOD = 100;
 
     // signal defenition
     reg clk;
     reg reset;
     reg  [COUNTER_BIT_NUM -1:0] counter;
-    wire  A  ;
-    wire  B  ;
-    wire  C_i;
-    wire  O  ;
-    wire  C_o;
-    assign A   = counter[0];
-    assign B   = counter[1];
-    assign C_i = counter[2];
+    wire  A;
+    wire  B;
+    wire  O;
+    wire  C;
+    assign A = counter[0];
+    assign B = counter[1];
 
     // dump wave file
     initial begin
         $dumpfile("tmp/dump.vcd");
-        $dumpvars(0, add1bit_1);
+        $dumpvars(0, add1bit_half_1);
     end
 
     // monitor
     initial begin
-        $monitor ("[%t] A=%b, B=%d, C_i=%d => O=%b C_o=%b", $time, 
-            add1bit_1.A  ,
-            add1bit_1.B  ,
-            add1bit_1.C_i,
-            add1bit_1.O  ,
-            add1bit_1.C_o);
+        $monitor ("[%t] A=%b, B=%d => O=%b C=%b", $time, 
+            add1bit_half_1.A,
+            add1bit_half_1.B,
+            add1bit_half_1.O,
+            add1bit_half_1.C);
     end
 
     // simulation end
@@ -64,12 +61,11 @@ module test_add1bit();
     end
 
 
-add1bit add1bit_1(
-    .A  (    A  ),
-    .B  (    B  ),
-    .C_i(    C_i),
-    .O  (    O  ),
-    .C_o(    C_o)
-);
+    add1bit_half add1bit_half_1 (
+        .A (A),
+        .B (B),
+        .O (O),
+        .C (C)
+        );
 
 endmodule
