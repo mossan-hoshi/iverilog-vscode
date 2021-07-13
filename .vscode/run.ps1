cba68d56e256d273abe0b756d40029b533783910
Chiro <chiro2001@163.com>
2021-07-13 09:12:37 +0800

    fix: replace `–` with `-`

--- .vscode/run.ps1
+++ .vscode/run.ps1
@@ -4,5 +4,5 @@ Param(
 )
 $log_file_path =  "log/$((Get-Date).ToString('yyyyMMdd_HHmmss')).log"
 $log_file_path
-Start-Process -Wait vvp.exe -ArgumentList "$vvp_file " –RedirectStandardOutput $log_file_path
+Start-Process -Wait vvp.exe -ArgumentList "$vvp_file " -RedirectStandardOutput $log_file_path
 gtkwave $wave_path
